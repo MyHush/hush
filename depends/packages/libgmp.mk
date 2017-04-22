eb3114644da2f8dbe913735ef79795af5be210ee
David Mercer <radix42@gmail.com>
2017-04-22 01:39:32 +0000

    changes for windows cross-compile

--- depends/packages/libgmp.mk
+++ depends/packages/libgmp.mk
@@ -1,8 +1,10 @@
 package=libgmp
-$(package)_version=6.1.1
-$(package)_download_path=https://gmplib.org/download/gmp/
-$(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=a8109865f2893f1373b0a8ed5ff7429de8db696fc451b1036bd7bdf95bbeffd6
+$(package)_download_path=https://github.com/joshuayabut/$(package)/archive/
+$(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
+$(package)_download_file=$($(package)_git_commit).tar.gz
+$(package)_sha256_hash=193836c1acc9dc00fe2521205d7bbe1ba13263f6cbef6f02584bf6f8b34b108f
+$(package)_git_commit=053c03b1cab347671d936f43ef66b48ab5e380ee
+
 $(package)_dependencies=
 $(package)_config_opts=--enable-cxx --disable-shared
 
--- depends/packages/libsnark.mk
+++ depends/packages/libsnark.mk
@@ -1,15 +1,14 @@
 package=libsnark
-$(package)_version=0.1
-$(package)_download_path=https://github.com/zcash/$(package)/archive/
+$(package)_download_path=https://github.com/radix42/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=dad153fe46e2e1f33557a195cbe7d69aed8b19ed9befc08ddcb8c6d3c025941f
-$(package)_git_commit=9ada3f84ab484c57b2247c2f41091fd6a0916573
+$(package)_sha256_hash=a7663783eb63bb59698d3997372b86a9d1b20dff5336b65fc9ddd9171c46c83d
+$(package)_git_commit=4015f558bb7a1b603de4ee1559c974389d3abb54
 
 $(package)_dependencies=libgmp libsodium
 
 define $(package)_build_cmds
-  CXXFLAGS="-fPIC -DBINARY_OUTPUT -DNO_PT_COMPRESSION=1" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
+  CXX="x86_64-w64-mingw32-g++-posix" CXXFLAGS="-DBINARY_OUTPUT -DPTW32_STATIC_LIB -DSTATICLIB -DNO_PT_COMPRESSION=1 -fopenmp" $(MAKE) lib DEPINST=$(host_prefix) CURVE=ALT_BN128 MULTICORE=1 NO_PROCPS=1 NO_GTEST=1 NO_DOCS=1 STATIC=1 NO_SUPERCOP=1 FEATUREFLAGS=-DMONTGOMERY_OUTPUT OPTFLAGS="-O2 -march=x86-64"
 endef
 
 define $(package)_stage_cmds
