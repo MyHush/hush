bcfeaebeb85e2a3c17a2f15de844528ae3c86b6c
David Mercer <radix42@gmail.com>
2017-06-19 12:06:32 +0000

    initial rebranded version for windows

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,5 @@
 rust_packages := rust librustzcash
-proton_packages := proton
+#proton_packages := proton
 zcash_packages := libsnark libgmp libsodium
 packages := boost openssl libevent $(zcash_packages) googletest googlemock
 native_packages := native_ccache
