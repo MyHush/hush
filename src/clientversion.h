e10fbfe2299adc3caf42583208eba199920a93a7
Matthew Reichardt <me@matthewreichardt.com>
2017-09-11 20:24:14 -0500

    Updated Copyright String
    I updated the copyright string to use proper grammar.

--- src/clientversion.h
+++ src/clientversion.h
@@ -40,7 +40,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers and The Zcash developers and The Hush developers"
+#define COPYRIGHT_STR "2009-2017" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core, Zcash, and Hush developers"
 
 /**
  * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
