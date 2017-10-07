00416efb31b300384a4c31fc4330f0da23f66814
csharpee <sharpetronics@gmail.com>
2017-10-07 23:51:55 +0000

    Updated rpcmining.cpp zcash to hush naming conventions

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -553,10 +553,10 @@ UniValue getblocktemplate(const UniValue& params, bool fHelp)
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
 
     if (vNodes.empty())
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "Zcash is not connected!");
+        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "Hush is not connected!");
 
     if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Zcash is downloading blocks...");
+        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "Hush is downloading blocks...");
 
     static unsigned int nTransactionsUpdatedLast;
 
