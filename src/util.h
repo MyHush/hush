49a851677096ba117bb358d0bc5c0a32e3bd5dad
Jonathan "Duke" Leto <jonathan@leto.net>
2017-10-07 22:51:52 -0700

    Make sure our threads are named hush-*

--- src/util.h
+++ src/util.h
@@ -227,7 +227,7 @@ void RenameThread(const char* name);
  */
 template <typename Callable> void TraceThread(const char* name,  Callable func)
 {
-    std::string s = strprintf("zcash-%s", name);
+    std::string s = strprintf("hush-%s", name);
     RenameThread(s.c_str());
     try
     {
