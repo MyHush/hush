a471fbe2c4cf366ba8306056889d506b7fd71063
FireMartZ <martindube@martindube.com>
2018-03-04 10:59:12 -0500

    Revert "WIP: Merge build files."
    This reverts commit 8783416c41284ab519e07879a19a45722debfd16.

--- depends/packages/googlemock.mk
+++ depends/packages/googlemock.mk
@@ -9,8 +9,8 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=release-$($(package)_version).tar.gz
 $(package)_sha256_hash=3f20b6acb37e5a98e8c4518165711e3e35d47deb6cdb5a4dd4566563b5efd232
 
-define $(package)_build_cmds
-  $(MAKE) -C make GTEST_DIR='$(host_prefix)' CXXFLAGS='-fPIC' gmock-all.o
+define $(package)_set_vars
+    $(package)_build_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)" CXX="$($(package)_cxx)" CXXFLAGS="$($(package)_cxxflags)"
 endef
 
 BUILD_OS := $(shell uname)
@@ -20,7 +20,11 @@ else
     $(package)_install=install
 endif
 
+define $(package)_build_cmds
+    $(MAKE) -C make GTEST_DIR='$(host_prefix)' gmock-all.o
+endef
+
 define $(package)_stage_cmds
-  install -D ./make/gmock-all.o $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
-  cp -a ./include $($(package)_staging_dir)$(host_prefix)/include
+    $($(package)_install) -D ./make/gmock-all.o $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
+    cp -a ./include $($(package)_staging_dir)$(host_prefix)/include
 endef
